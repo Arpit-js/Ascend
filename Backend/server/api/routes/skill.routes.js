39645ffbf1412398ee3abab33798aa62962bac39
Arpit-js <arxcodes.js@gmail.com>
2025-10-20 18:46:53 +0530

    Delete Backend/server directory

--- Backend/server/api/routes/skill.routes.js
+++
@@ -1,9 +0,0 @@
-// /server/api/routes/skill.routes.js
-const express = require('express');
-const router = express.Router();
-const skillController = require('../controllers/skill.controller');
-
-// GET /api/skills/
-router.get('/', skillController.getAllSkills);
-
-module.exports = router;
