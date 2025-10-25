06f2788115906ee0ed3640d2ec5e098eef142622
Arpit-js <arxcodes.js@gmail.com>
2025-10-25 20:04:02 +0530

    Delete Frontend/client directory

--- Frontend/client/src/components/common/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './Spinner.module.css';
-
-const Spinner = ({ size = 'medium' }) => {
-  const sizeClass = styles[size] || styles.medium;
-  return <div className={`${styles.spinner} ${sizeClass}`}></div>;
-};
-
-export default Spinner;
